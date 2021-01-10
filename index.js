5d8a91a5fbc7534742708947571318be45c0f14c
Asjid Kalam <asjid.kalam@gmail.com>
2021-01-10 16:22:48 +0530

    fixed prototype pollution

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ const getEnumerableOwnPropertyKeys = value => {
 	const keys = [];
 
 	for (const key in value) {
+		// prototype pollution mitigation
+		if(key.includes('__proto__') || key.includes('constructor') || key.includes('prototype')){
+			return keys;
+		}
 		if (hasOwnProperty.call(value, key)) {
 			keys.push(key);
 		}
